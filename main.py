4037d7bebb7a2b870727bb9950a1b044e3f57ca6
Ricardo Dematte <103331571+ricardodematte@users.noreply.github.com>
2022-05-02 19:05:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Elabore um algoritmo que leia três valores inteiros.
+Mostre a soma destes valores.
+'''
+
+import os
+n1 = (int(input("Digite um número inteiro n1:\n")))
+n2 = (int(input("Digite um número inteiro n2:\n")))
+n3 = (int(input("Digite um número inteiro n3:\n")))
+soma = n1+n2+n3
+os.system("clear")
+print ("A soma é ",soma)
